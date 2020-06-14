45d15ada8d8d26716e9f01cf78244da6343e2eff
wanggc@dehuinet.com <wanggc@dehuinet.com>
2020-06-14 13:31:22 +0800

    dev-101

--- src/test.java
+++ src/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
+        System.out.println("dev-100");
+
         System.out.println("订单");
+
+        System.out.println("dev-101");
     }
 }
